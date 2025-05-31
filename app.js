00bc408cc27543d8748e0b9b22aefb31852e6f95
Ruvindu <ruvindusharada22@gmail.com>
2025-05-31 20:59:40 +0530

    create the aplication json

--- app.js
+++ app.js
@@ -1,52 +1,92 @@
-const { log } = require('console');
-var {sum,divide} = require('./calculate');
-const http= require('http');
-const path = require('path');
-const fs = require('fs');
-const PORT = 3000;
+// const { log } = require('console');
+// var {sum,divide} = require('./calculate');
+// const http= require('http');
+// const path = require('path');
+// const fs = require('fs');
+
+
+const express=require('express');
 const productService=require('./Product');
+const bodyParser = require('body-parser');
+const { log } = require('console');
+const { Server } = require('http');
+const app = express();
+const PORT= process.env.PORT || 3000;
+app.use(bodyParser.json());
 
-const server = http.createServer((req, res) => {
-    if(req.method === 'GET' && req.url === '/create-user') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    const user = {
-        name: "ruvindu",
-        age: 23,
-        address: "Colombo",
-        email: "ruvindu@example.com"
-    };
-    res.end(JSON.stringify(user));
-    }
 
-    if(req.method === 'POST' && req.url === '/create-user-json') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        const user = {
-            name: "ruvindu",
-            age: 23,
-            address: "Colombo",
-            email: "ruvindu@example.com"
-    };
-    res.end(JSON.stringify(user)
-);
-};
 
-if(req.method === 'GET' && req.url === '/products') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+});
+
+app.post('/product/create/:id', (req, res) => {
+    let body = req.body;
+    let id = req.params.id;
+    console.log(id);
+    console.log(body);
+
+    res.send(body);
+});
+app.put('/product/update/:id', (req, res) => {
+    let body = req.body;
+    let id = req.params.id;
+    
+    res.send(body);
+    console.log("Product updated:", id);
+
+    console.log("Product updated:", body);
+});
+
+app.get('/products', (req, res) => {
     const products = productService.getProduct;
-    log('Products:', products);
-    res.end(JSON.stringify(products));
-}
-
+    console.log('Products:', products);
+    res.json(products);
 });
 
 
 
 
+// console.log("Sum:", sum(10, 20));
+// console.log("Divide:", divide(10, 20));
+
+// const server = http.createServer((req, res) => {
+//     if(req.method === 'GET' && req.url === '/create-user') {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     const user = {
+//         name: "ruvindu",
+//         age: 23,
+//         address: "Colombo",
+//         email: "ruvindu@example.com"
+//     };
+//     res.end(JSON.stringify(user));
+//     }
+
+//     if(req.method === 'POST' && req.url === '/create-user-json') {
+//         res.writeHead(200, { 'Content-Type': 'application/json' });
+//         const user = {
+//             name: "ruvindu",
+//             age: 23,
+//             address: "Colombo",
+//             email: "ruvindu@example.com"
+//     };
+//     res.end(JSON.stringify(user)
+// );
+// };
+
+// if(req.method === 'GET' && req.url === '/products') {
+//     res.writeHead(200, { 'Content-Type': 'application/json' });
+//     const products = productService.getProduct;
+//     log('Products:', products);
+//     res.end(JSON.stringify(products));
+// }
+
+// });
 
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
 
 
 
